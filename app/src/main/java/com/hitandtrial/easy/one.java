0e45348df0fd1033b9ad03ebb7da5f261775ef8f
Himanshu Pandey <himanshu.pandey.5011@gmail.com>
2019-08-10 23:25:30 +0530

    Initial commit

---
+++ app/src/main/java/com/hitandtrial/easy/one.java
@@ -0,0 +1,13 @@
+package com.hitandtrial.easy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class one extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one);
+    }
+}
